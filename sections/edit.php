78e9936008582c6a29c8c031d52be5b932f2780b
Christophe Battarel <tof@localhost.(none)>
2010-09-27 23:26:17 +0200

    Add surfer editor for introduction and extra content while editing articles, sections and categories

--- sections/edit.php
+++ sections/edit.php
@@ -17,7 +17,7 @@
  * @tester Marco Pici
  * @tester Ghjmora
  * @tester Aleko
- * @tester Manuel López Gallego
+ * @tester Manuel Lï¿½pez Gallego
  * @tester Jan Boen
  * @reference
  * @license http://www.gnu.org/copyleft/lesser.txt GNU Lesser General Public License
@@ -336,7 +336,8 @@ if($with_form) {
 	// the introduction
 	if(!is_object($overlay) || !($label = $overlay->get_label('introduction', isset($item['id'])?'edit':'new')))
 		$label = i18n::s('Introduction');
-	$input = '<textarea name="introduction" rows="2" cols="50" accesskey="i">'.encode_field(isset($item['introduction']) ? $item['introduction'] : '').'</textarea>';
+	//$input = '<textarea name="introduction" rows="2" cols="50" accesskey="i">'.encode_field(isset($item['introduction']) ? $item['introduction'] : '').'</textarea>';
+	$input = Surfer::get_editor('introduction', isset($item['introduction'])?$item['introduction']:'');
 	if(!is_object($overlay) || !($hint = $overlay->get_label('introduction_hint', isset($item['id'])?'edit':'new')))
 		$hint = i18n::s('Appears at the site map, near section title');
 	$fields[] = array($label, $input, $hint);
@@ -644,7 +645,8 @@ if($with_form) {
 
 	// extra information
 	$label = i18n::s('Extra');
-	$input = '<textarea name="extra" rows="6" cols="50">'.encode_field(isset($item['extra']) ? $item['extra'] : '').'</textarea>';
+	//$input = '<textarea name="extra" rows="6" cols="50">'.encode_field(isset($item['extra']) ? $item['extra'] : '').'</textarea>';
+	$input = Surfer::get_editor('extra', isset($item['extra'])?$item['extra']:'');
 	$hint = i18n::s('Text to be inserted in the panel aside the page. Use [box.extra=title]content[/box] or plain HTML.');
 	$fields[] = array($label, $input, $hint);
 
