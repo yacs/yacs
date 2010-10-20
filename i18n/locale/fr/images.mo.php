f73bd1066aae65345b8ba12d10468554b65823b1
Bernard Paques <bernard.paques@gmail.com>
2010-10-20 08:52:08 +0200

    i18n update

--- i18n/locale/fr/images.mo.php
+++ i18n/locale/fr/images.mo.php
@@ -7,7 +7,7 @@
  */
 $_SESSION['l10n']['fr']['_headers']='Project-Id-Version: YACS
 Report-Msgid-Bugs-To: 
-POT-Creation-Date: 2010-09-29 14:20+0200
+POT-Creation-Date: 2010-10-19 22:05+0200
 PO-Revision-Date: 2010-08-11 17:31+0100
 Last-Translator: Bernard Paques
 Language-Team: Agnès Rambaud, Bernard Paques
