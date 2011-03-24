2ad82d3be3d4a973322ba8c9b73e5f38bfd3924b
Bernard Paques <bernard.paques@gmail.com>
2011-03-24 23:02:57 +0100

    group addition commands in page menu in user profiles

--- users/view.php
+++ users/view.php
@@ -329,6 +329,9 @@ if(!isset($item['id'])) {
 			(Surfer::is_associate() || ($context['users_maximum_managed_sections'] > Sections::count_for_owner())) ) {
 			Skin::define_img('SECTIONS_ADD_IMG', 'sections/add.gif');
 			$box['top'] += array('sections/new.php' => SECTIONS_ADD_IMG.i18n::s('Add some space for your pages'));
+
+			// make it visible at page top
+			$context['page_menu'] += array('sections/new.php' => SECTIONS_ADD_IMG.i18n::s('Add some space for your pages'));
 		}
 
 		// associates can assign editors and readers
@@ -395,6 +398,9 @@ if(!isset($item['id'])) {
 				Skin::define_img('ARTICLES_ADD_IMG', 'articles/add.gif');
 				$url = 'articles/edit.php?anchor='.urlencode($reference);
 				$box['top'] += array($url => ARTICLES_ADD_IMG.i18n::s('Add a standalone page'));
+
+				// make it visible at page top
+				$context['page_menu'] += array($url => ARTICLES_ADD_IMG.i18n::s('Add a standalone page'));
 			}
 
 		}
