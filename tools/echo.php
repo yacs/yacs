0bcaec710d5969edd4bf7d3f1900e1d43e468f01
Bernard Paques <bernard.paques@gmail.com>
2011-04-28 23:46:50 +0200

    new codes to include external script and to redirect to a local page

---
+++ tools/echo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * to simulate a script generating some text
+ *
+ * @author Bernard Paques
+ * @reference
+ * @license http://www.gnu.org/copyleft/lesser.txt GNU Lesser General Public License
+ */
+
+// display page content
+echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		.' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		.' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+		.' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+
+?>
