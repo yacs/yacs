159cdc01baabe8e38bdb0cdf6fa6e7aefa9c5362
Alexis Raimbault <rair@cybermedium.info>
2011-06-16 10:05:42 +0200

    [jquery] add posibility to load extra js libraries after scanning a folder, caching result in database, function to add occasional js file, adapting call of libs in global.php

--- shared/js_css.php
+++ shared/js_css.php
@@ -11,22 +11,76 @@
 Class Js_Css {
 
     /**
-     * return metatags to declare external js libraries
+     * add declaration for occasionnal external javascript file
      * @see shared/global.php
      *
-     * @param string folder where to search js file, from included/browsers
-     * @return string
+     * @param string relative path to local script file
+     * @param string place in page to load the script, "footer" or "header"
+     * @return <type> 
      */
-    function get_js_libraries($folder='') {
+    function add_external_js($path,$target='footer') {
+	global $context;
+	
+	if(!file_exists($context['path_to_root'].$path))
+	    return;
+
+	if(!isset($context['javascript'][$target]))
+	    $context['javascript'][$target] = '';
+
+	$html = Js_Css::build_js_declaration($context['url_to_root'].$path);
+	$context['javascript'][$target] .= $html;
+    }
+
+    function add_inline_js($script_text,$target='footer') {
+
+    }
+
+    function build_css_declaration($path,$media="all") {
+
+	$html = '<link rel="stylesheet" href="'.$path.'"type="text/css" media="'.$media.'" />'."\n";
+	return $html;
+    }
+
+    /**
+     * build html declaration to a js file
+     * 
+     * @todo select XHTML or HTML5 format
+     * 
+     * @param string path to the file
+     * @return string 
+     */
+    function build_js_declaration($path) {
+
+	$html = '<script type="text/javascript" src="'.$path.'"></script>'."\n";
+	return $html;
+    }
+
+    /**
+     * return <script> html tags to declare external js libraries
+     * @see shared/global.php
+     *
+     * @todo minify&merge files in production mode
+     *
+     * @param string, folder where to search js file, from included/browsers
+     * @param string or array of strings, relative path to other libraries
+     * to deal with (e.g. shared/yacs.js)
+     * @return string in html format
+     */
+    function get_js_libraries($folder='',$other_files='') {
 	global $context;
 
 	// we provide html tags links to scripts files
-	$tags = '';
+	$html = '';
+
+	// cache this across requests
+	$cache_id = 'shared/js_css.php#lib_'.$folder;
+	if($html =& Cache::get($cache_id))
+	    return $html;
 	
 	// path to search for js file, default is "include/browser"
 	$path = 'included/browser/'.(($folder)?$folder.'/':'');
 
-	// scan for js files
+	// scan for js files in folder
 	if($dir = Safe::opendir($path)) {
 
 	    $js_libs = array();
@@ -44,25 +98,26 @@ Class Js_Css {
 	if($js_libs) {
 	    // files can be renamed with a letters prefix to sort loading from browsers
 	    natsort($js_libs);
-	    // build declaration
-	    // TODO minify&merge in production mode
-	    // TODO select XHTML or HTML5 format
+	    // build declaration	    
 	    foreach ($js_libs as $js) {
-		$tags .= '<script type="text/javascript" src="'
-			.$context['url_to_root'].$path.$js
-			.'"></script>'."\n";
+		$html .= Js_Css::build_js_declaration($context['url_to_root'].$path.$js);
 	    }
 	}
 
-	return $tags;
-    }
+	if($other_files) {
+	    if(gettype($other_files)=='string')
+		$other_files = (array) $other_files;
 
-    function add_external_js($script_path,$target='footer') {
+	    foreach ($other_files as $file) {
+		if(file_exists($context['path_to_root'].$file))
+		    $html .= Js_Css::build_js_declaration($context['url_to_root'].$file);
+	    }
 
-    }
+	}
 
-    function add_inline_js($script_text,$target='footer') {
-
+	// cache for 1 month
+	Cache::put($cache_id, $html, 'stable',2592000);
+	return $html;
     }
 }
 ?>
