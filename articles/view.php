a48386bc87f2d97d0f0cead9a7c9b3b1176472a4
Bernard Paques <bernard.paques@gmail.com>
2011-08-13 22:25:04 +0200

    activate the command to notify participants from regular articles

--- articles/view.php
+++ articles/view.php
@@ -588,6 +588,12 @@ if(!isset($item['id'])) {
 		$lines[] = Skin::build_link(Articles::get_url($item['id'], 'invite'), ARTICLES_INVITE_IMG.i18n::s('Invite participants'), 'basic', i18n::s('Spread the word'));
 	}
 
+	// notify participants
+	if((Articles::is_owned($item, $anchor) || Surfer::is_associate()) && isset($context['with_email']) && ($context['with_email'] == 'Y')) {
+		Skin::define_img('ARTICLES_EMAIL_IMG', 'articles/email.gif');
+		$lines[] = Skin::build_link(Sections::get_url($item['id'], 'mail'), ARTICLES_EMAIL_IMG.i18n::s('Notify participants'));
+	}
+
 	// manage editors
  	if(Articles::is_owned($item, $anchor, TRUE) || Surfer::is_associate()) {
  		Skin::define_img('ARTICLES_ASSIGN_IMG', 'articles/assign.gif');
