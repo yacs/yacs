aa936079163f8d2b7f18a185498fcfde104c9d20
Alexis Raimbault <rair@cybermedium.info>
2011-09-21 10:34:14 +0200

    complete mo.php building

---
+++ i18n/locale/en/smileys.mo.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * cache localized strings
+ *
+ * This file has been created by the script i18n/i18n.php. Please do not modify it manually.
+ * @reference
+ */
+$_SESSION['l10n']['en']['_headers']='Project-Id-Version: YACS
+Report-Msgid-Bugs-To: 
+POT-Creation-Date: 2011-09-21 10:03+0200
+PO-Revision-Date: 2008-06-28 10:39+0100
+Last-Translator: Paques, Bernard
+Language-Team: Bernard Paques
+Language: 
+MIME-Version: 1.0
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: 8bit
+Plural-Forms: nplurals=2; plural=(n != 1);
+X-Poedit-Language: English
+X-Poedit-Country: UNITED KINGDOM
+X-Poedit-SourceCharset: utf-8
+';
+$_SESSION['l10n']['en']['Edited %s']='Edited %s';
+$_SESSION['l10n']['en']['Emotion']='Emotion';
+$_SESSION['l10n']['en']['Featured image']='Featured image';
+$_SESSION['l10n']['en']['Help index']='Help index';
+$_SESSION['l10n']['en']['fbcdb634789b1a0dc26cd32139230ac0']='Here is the list of codes that are automatically converted into images by this server.';
+$_SESSION['l10n']['en']['I am killed']='I am killed';
+$_SESSION['l10n']['en']['I want you']='I want you';
+$_SESSION['l10n']['en']['I would like to get it']='I would like to get it';
+$_SESSION['l10n']['en']['e6ef69399a84e48349e980587fd573b1']='Let face it, sometimes words alone do not suffice. Adding a winking smilie, for instance, may help you clarify that you are joking.';
+$_SESSION['l10n']['en']['Ok, i leave']='Ok, i leave';
+$_SESSION['l10n']['en']['Santa Claus']='Santa Claus';
+$_SESSION['l10n']['en']['Smileys']='Smileys';
+$_SESSION['l10n']['en']['afbff2832f64949ef35c8901a6120a58']='Smileys are small graphical images that can be used to convey an emotion or feeling. If you have used email or Internet chat, you are likely familiar with the smilie concept.';
+$_SESSION['l10n']['en']['Star wars']='Star wars';
+$_SESSION['l10n']['en']['The King']='The King';
+$_SESSION['l10n']['en']['96e88b57640c84ef5eb9808ef98922c5']='Use your smilies sparingly, though -- if overused, smilies can be downright annoying.';
+$_SESSION['l10n']['en']['What to type']='What to type';
+$_SESSION['l10n']['en']['a cup of coffee']='a cup of coffee';
+$_SESSION['l10n']['en']['a little nap']='a little nap';
+$_SESSION['l10n']['en']['alien']='alien';
+$_SESSION['l10n']['en']['angel']='angel';
+$_SESSION['l10n']['en']['angry']='angry';
+$_SESSION['l10n']['en']['asian salutation']='asian salutation';
+$_SESSION['l10n']['en']['bad']='bad';
+$_SESSION['l10n']['en']['bla bla bla']='bla bla bla';
+$_SESSION['l10n']['en']['bomb']='bomb';
+$_SESSION['l10n']['en']['bounce']='bounce';
+$_SESSION['l10n']['en']['candle']='candle';
+$_SESSION['l10n']['en']['cat']='cat';
+$_SESSION['l10n']['en']['cheesy']='cheesy';
+$_SESSION['l10n']['en']['clap clap clap, etc.']='clap clap clap, etc.';
+$_SESSION['l10n']['en']['clock']='clock';
+$_SESSION['l10n']['en']['colored']='colored';
+$_SESSION['l10n']['en']['confused']='confused';
+$_SESSION['l10n']['en']['cry']='cry';
+$_SESSION['l10n']['en']['dog']='dog';
+$_SESSION['l10n']['en']['ekk/horror!']='ekk/horror!';
+$_SESSION['l10n']['en']['email']='email';
+$_SESSION['l10n']['en']['embarassed']='embarassed';
+$_SESSION['l10n']['en']['enough is enough']='enough is enough';
+$_SESSION['l10n']['en']['feeling bad']='feeling bad';
+$_SESSION['l10n']['en']['female']='female';
+$_SESSION['l10n']['en']['film']='film';
+$_SESSION['l10n']['en']['flush and forget it']='flush and forget it';
+$_SESSION['l10n']['en']['frog']='frog';
+$_SESSION['l10n']['en']['frown']='frown';
+$_SESSION['l10n']['en']['ghost']='ghost';
+$_SESSION['l10n']['en']['god bless you']='god bless you';
+$_SESSION['l10n']['en']['good']='good';
+$_SESSION['l10n']['en']['good bye']='good bye';
+$_SESSION['l10n']['en']['have a cocktail']='have a cocktail';
+$_SESSION['l10n']['en']['have a party']='have a party';
+$_SESSION['l10n']['en']['hip']='hip';
+$_SESSION['l10n']['en']['hot bounce']='hot bounce';
+$_SESSION['l10n']['en']['i am feeling so glad']='i am feeling so glad';
+$_SESSION['l10n']['en']['f4c20e65fe7113eb598fc66650f86bac']='i want to be sure you are hearing me';
+$_SESSION['l10n']['en']['i will kill you']='i will kill you';
+$_SESSION['l10n']['en']['i will shot you']='i will shot you';
+$_SESSION['l10n']['en']['idea']='idea';
+$_SESSION['l10n']['en']['it stinks']='it stinks';
+$_SESSION['l10n']['en']['joker']='joker';
+$_SESSION['l10n']['en']['kiss']='kiss';
+$_SESSION['l10n']['en']['let have some beer']='let have some beer';
+$_SESSION['l10n']['en']['lovely']='lovely';
+$_SESSION['l10n']['en']['male']='male';
+$_SESSION['l10n']['en']['note']='note';
+$_SESSION['l10n']['en']['once upon a time...']='once upon a time...';
+$_SESSION['l10n']['en']['operating systems']='operating systems';
+$_SESSION['l10n']['en']['peace']='peace';
+$_SESSION['l10n']['en']['peeping']='peeping';
+$_SESSION['l10n']['en']['pet']='pet';
+$_SESSION['l10n']['en']['phone']='phone';
+$_SESSION['l10n']['en']['pissed off']='pissed off';
+$_SESSION['l10n']['en']['please stop']='please stop';
+$_SESSION['l10n']['en']['present']='present';
+$_SESSION['l10n']['en']['question']='question';
+$_SESSION['l10n']['en']['reading']='reading';
+$_SESSION['l10n']['en']['roll eyes']='roll eyes';
+$_SESSION['l10n']['en']['rolling on the floor, laughing']='rolling on the floor, laughing';
+$_SESSION['l10n']['en']['rose']='rose';
+$_SESSION['l10n']['en']['sealed']='sealed';
+$_SESSION['l10n']['en']['shaky brain']='shaky brain';
+$_SESSION['l10n']['en']['share a cake']='share a cake';
+$_SESSION['l10n']['en']['shocked']='shocked';
+$_SESSION['l10n']['en']['smile']='smile';
+$_SESSION['l10n']['en']['sniping']='sniping';
+$_SESSION['l10n']['en']['some electronic inside']='some electronic inside';
+$_SESSION['l10n']['en']['star']='star';
+$_SESSION['l10n']['en']['sunglass dude']='sunglass dude';
+$_SESSION['l10n']['en']['sunshine']='sunshine';
+$_SESSION['l10n']['en']['take a shot']='take a shot';
+$_SESSION['l10n']['en']['the dark side of The Force']='the dark side of The Force';
+$_SESSION['l10n']['en']['thinking']='thinking';
+$_SESSION['l10n']['en']['this deserves a full medal']='this deserves a full medal';
+$_SESSION['l10n']['en']['this deserves half of a medal']='this deserves half of a medal';
+$_SESSION['l10n']['en']['this does not make sense']='this does not make sense';
+$_SESSION['l10n']['en']['this is an unsafe place']='this is an unsafe place';
+$_SESSION['l10n']['en']['to the basket']='to the basket';
+$_SESSION['l10n']['en']['tongue']='tongue';
+$_SESSION['l10n']['en']['undecided']='undecided';
+$_SESSION['l10n']['en']['unlove']='unlove';
+$_SESSION['l10n']['en']['warning']='warning';
+$_SESSION['l10n']['en']['we will hit the wall']='we will hit the wall';
+$_SESSION['l10n']['en']['wink/sarcasm']='wink/sarcasm';
+$_SESSION['l10n']['en']['with (more) love']='with (more) love';
+$_SESSION['l10n']['en']['with love']='with love';
+$_SESSION['l10n']['en']['yin yang']='yin yang';
+$_SESSION['l10n']['en']['you are so naive']='you are so naive';
+$_SESSION['l10n']['en']['you make me blush']='you make me blush';
+$_SESSION['l10n']['en']['you should not do that']='you should not do that';
+$_SESSION['l10n']['en']['_plural']='nplurals=2; plural=(n != 1);';
+?>
---
+++ i18n/locale/fr/services.mo.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * cache localized strings
+ *
+ * This file has been created by the script i18n/i18n.php. Please do not modify it manually.
+ * @reference
+ */
+$_SESSION['l10n']['fr']['_headers']='Project-Id-Version: YACS
+Report-Msgid-Bugs-To: 
+POT-Creation-Date: 2011-09-21 10:03+0200
+PO-Revision-Date: 2010-04-08 22:58+0100
+Last-Translator: Bernard Paques
+Language-Team: Agnès Rambaud, Bernard Paques
+Language: 
+MIME-Version: 1.0
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: 8bit
+Plural-Forms: nplurals=2; plural=(n != 1);
+X-Poedit-Language: French
+X-Poedit-Country: FRANCE
+X-Poedit-SourceCharset: utf-8
+';
+$_SESSION['l10n']['fr']['%s at %s']='%s à %s';
+$_SESSION['l10n']['fr']['%s has been updated']='%s a été mis à jour';
+$_SESSION['l10n']['fr']['%s: %s']='%s : %s';
+$_SESSION['l10n']['fr']['Back-end services']='Services distribués';
+$_SESSION['l10n']['fr']['Blogging interface']='Interface pour le webblogging';
+$_SESSION['l10n']['fr']['0c8d5a7365a5761e04f27e7c2dac47f0']='Liée aux scripts de service';
+$_SESSION['l10n']['fr']['c0ccde3ba07ca4ee6dd86de92c1bd748']='Pour bâtir un fil de nouvelles RSS à partir de mots de recherche';
+$_SESSION['l10n']['fr']['Configuration parameters']='Paramètres de configuration';
+$_SESSION['l10n']['fr']['Configure']='Configuration';
+$_SESSION['l10n']['fr']['Configure again']='Configurer de nouveau';
+$_SESSION['l10n']['fr']['Control Panel']='Panneau de contrôle';
+$_SESSION['l10n']['fr']['5ce66149f67e5a6de682eef4da0b9182']='Sauver les paquets envoyés et reçus par comments/post.php dans le fichier temporary/debug.txt';
+$_SESSION['l10n']['fr']['00882556621314b26d58a0c117b6bd7d']='Sauver les paquets envoyés et reçus par links/trackback.php dans le fichier temporary/debug.txt';
+$_SESSION['l10n']['fr']['c0f5a68c9c04aab5406a465b02eb1dca']='Sauver les paquets envoyés et reçus par services/blog.php dans le fichier temporary/debug.txt';
+$_SESSION['l10n']['fr']['7295f889c651091fdf28ad6e05a9963a']='Sauver les paquets envoyés et reçus par services/call.php dans le fichier temporary/debug.txt';
+$_SESSION['l10n']['fr']['a22aa9ef5d4372c0425c6002843f9e1e']='Sauver les paquets envoyés et reçus par services/json_rpc.php et par services/xml_rpc.php dans le fichier temporary/debug.txt';
+$_SESSION['l10n']['fr']['43d7213ac181800ee7f59b994e4d803e']='Sauver les paquets envoyés et reçus par services/ping.php dans le fichier temporary/debug.txt';
+$_SESSION['l10n']['fr']['Debug blog services']='Mode de mise au point des services blogs';
+$_SESSION['l10n']['fr']['Debug calls of web services']='Mode de mise au point des appels client';
+$_SESSION['l10n']['fr']['Debug commenting services']='Mode de mise au point des services de commentaire';
+$_SESSION['l10n']['fr']['Debug ping services']='Mode de mise au point des services ping';
+$_SESSION['l10n']['fr']['393bc438016c915ff159e90b65b4ebb8']='Mode de mise au point des services pingback et trackback';
+$_SESSION['l10n']['fr']['Debug remote procedure calls']='Mode de mise au point des appels à distance (RPC)';
+$_SESSION['l10n']['fr']['Do not know how to process %s']='%s ne correspond à rien de connu';
+$_SESSION['l10n']['fr']['Documentation:']='Documentation :';
+$_SESSION['l10n']['fr']['6a2c769d729b50a589114cb6f3a6ca3b']='ERREUR : impossible d\'écrire dans le fichier %s. La configuration n\'a pas été sauvegardée.';
+$_SESSION['l10n']['fr']['Generic JSON-RPC interface']='Interface JSON-RPC générique';
+$_SESSION['l10n']['fr']['Generic XML-RPC interface']='Interface XML-RPC générique';
+$_SESSION['l10n']['fr']['Go']='Go';
+$_SESSION['l10n']['fr']['37cc548a33139f309e8e319d6ab3bf6b']='Allez dans le %s et assurez-vous que le mode de mise au point pour les <code>call</code> a été sélectionné si vous souhaitez enregistrer les requêtes et réponses HTTP.';
+$_SESSION['l10n']['fr']['deb98ab230081a96a6dd968bd9724d0c']='Allez dans le %s et assurez-vous que le mode de mise au point pour les <code>rpc</code> a été sélectionné si vous souhaitez enregistrer les requêtes et réponses HTTP.';
+$_SESSION['l10n']['fr']['Google API key']='Clé d\'API Google';
+$_SESSION['l10n']['fr']['Google Analytics']='Google Analytics';
+$_SESSION['l10n']['fr']['Help']='Aide';
+$_SESSION['l10n']['fr']['5c50fcbf1b26d4dcdc72b133903986ac']='Impossible d\'effacer l\'envoi %s';
+$_SESSION['l10n']['fr']['d3b4dc64a8f41b7745b998b11b593e08']='Impossible d\'enregistrer le contenu du fichier %s';
+$_SESSION['l10n']['fr']['0d52428919bfb9842e2a87c81077a76e']='Impossible de modifier la page %s';
+$_SESSION['l10n']['fr']['Message']='Message';
+$_SESSION['l10n']['fr']['Name of the updated server']='Nom du serveur mis à jour';
+$_SESSION['l10n']['fr']['No anchor has been found.']='Aucun ancrage n\'a été trouvé.';
+$_SESSION['l10n']['fr']['No file data has been received.']='Aucun fichier n\'a été transmis.';
+$_SESSION['l10n']['fr']['Ping interface']='Interface Ping';
+$_SESSION['l10n']['fr']['32f16b405d7ec3c7f7a2175487b60cc3']='Merci de vous enregistrer à l\'adresse %s avant de blogger';
+$_SESSION['l10n']['fr']['Press [s] to submit data']='Appuyer sur [s] pour envoyer les informations';
+$_SESSION['l10n']['fr']['Sample blog client']='Exemple de client blog';
+$_SESSION['l10n']['fr']['Sample ping client']='Exemple de client ping';
+$_SESSION['l10n']['fr']['Search']='Recherche';
+$_SESSION['l10n']['fr']['Server address']='Adresse du serveur';
+$_SESSION['l10n']['fr']['Specification:']='Spécification :';
+$_SESSION['l10n']['fr']['Specifications:']='Spécifications :';
+$_SESSION['l10n']['fr']['Submit']='Enregistrer';
+$_SESSION['l10n']['fr']['706311ed9eb6ffea0453c7a87556aa5e']='Pour le support de logiciels de blogging usuels tels que w.bloggar.';
+$_SESSION['l10n']['fr']['Test pingback.ping']='Test pingback.ping';
+$_SESSION['l10n']['fr']['Test weblogUpdates.ping']='Test weblogUpdates.ping';
+$_SESSION['l10n']['fr']['3513760fe3e0b14f2af4cc30a501d92a']='La configuration suivante a été enregistrée dans le fichier %s.';
+$_SESSION['l10n']['fr']['2be1d54d98311704480cb75da8aa9534']='Le nom ou l\'adresse IP du serveur cible';
+$_SESSION['l10n']['fr']['dda86db9a2409f98e6e9438cbc5e6cd7']='Cette page liste les principaux services que vous pouvez utiliser en arrière-plan. Connectez votre logiciel de blogging, votre lecteur de nouvelles RSS, ou d\'autres sites à ce serveur web sur la base des interfaces programmatiques décrites ci-dessous.';
+$_SESSION['l10n']['fr']['2879e4072ff5d8ea656b127262875171']='Pour changer la configuration, merci de copier et de coller les lignes suivantes dans le fichier %s :';
+$_SESSION['l10n']['fr']['acd27d20c6abc6cc445b677d8046f973']='Pour intégrer Google Maps à votre serveur, %s et indiquez-la ici';
+$_SESSION['l10n']['fr']['5a363027fb4f6b0bede10ded8fcf46c8']='To monitor your server with Google Analytics, %s and enter your account here';
+$_SESSION['l10n']['fr']['To spread updates']='Pour diffuser les mises à jour';
+$_SESSION['l10n']['fr']['URI of the source page']='Adresse de la page source';
+$_SESSION['l10n']['fr']['URI of the target page']='Adresse de la page cible';
+$_SESSION['l10n']['fr']['URI of the updated server']='Adresse du serveur mis à jour';
+$_SESSION['l10n']['fr']['URL:']='Adresse :';
+$_SESSION['l10n']['fr']['Unknown blog %s at %s']='Identifiant blog %s inconnu à l\'adresse %s';
+$_SESSION['l10n']['fr']['Unknown postid %s at %s']='Identifiant postid %s inconnu à l\'adresse %s';
+$_SESSION['l10n']['fr']['Unknown user name']='Nom d\'utilisateur inconnu';
+$_SESSION['l10n']['fr']['1067d44616fe8e1d0ba70d8a76f038fa']='A réserver aux phases de mise au point';
+$_SESSION['l10n']['fr']['User name']='Nom d\'utilisateur';
+$_SESSION['l10n']['fr']['User password']='Mot de passe';
+$_SESSION['l10n']['fr']['Web service test']='Test de service web';
+$_SESSION['l10n']['fr']['Web services']='Services web';
+$_SESSION['l10n']['fr']['Where do you want to go now?']='Où voulez-vous aller à présent ?';
+$_SESSION['l10n']['fr']['f2dc676b7cd35c9c7c975703bad0824c']='Vous n\'êtes pas autorisé à faire cette opération en mode démonstration.';
+$_SESSION['l10n']['fr']['3a7e4261f6cce22de8c07d2856dcc0f7']='Vous n\'êtes pas autorisé à faire cette opération.';
+$_SESSION['l10n']['fr']['apply for a key']='demandez une clé';
+$_SESSION['l10n']['fr']['a1d3a86cb66f6642e23c2f5f12323187']='le panneau de configuration des services';
+$_SESSION['l10n']['fr']['6889e5b8e03c320d08c3687b4e284a3e']='http://www.google-analytics.com/';
+$_SESSION['l10n']['fr']['8347fa7902ce30c1e25d97ec6d1ba222']='http://www.google.com/apis/maps/signup.html';
+$_SESSION['l10n']['fr']['register']='enregistrez-vous';
+$_SESSION['l10n']['fr']['_plural']='nplurals=2; plural=(n != 1);';
+?>
