7e3d9c1a3b69bdc6fec0f0fcc502baa12c37143b
Bernard Paques <bernard.paques@gmail.com>
2011-10-06 23:18:15 +0200

    [fix] order alphabetically images uploaded in an archive files

--- images/upload.php
+++ images/upload.php
@@ -11,6 +11,7 @@
  * @author Bernard Paques
  * @author Christophe Battarel
  * @tester Alexis Raimbault
+ * @tester Daniel Dupuis
  * @reference
  * @license http://www.gnu.org/copyleft/lesser.txt GNU Lesser General Public License
  */
@@ -193,6 +194,7 @@ if(Surfer::is_crawler()) {
 		if($handle = Safe::opendir($file_path)) {
 
 			// list all nodes
+			$nodes = array();
 			while(($node = Safe::readdir($handle)) !== FALSE) {
 
 				// special directory names
@@ -224,12 +226,17 @@ if(Surfer::is_crawler()) {
 					}
 
 					// ensure that the image is in anchor description field
-					if(isset($item['id']))
-						$anchor->touch('image:create', $item['id']);
+					$nodes[ $node ] = $item['id'];
 
 				}
 			}
 			Safe::closedir($handle);
+
+			// embed uploaded images in alphabetical order
+			ksort($nodes);
+			foreach($nodes as $name => $id)
+				$anchor->touch('image:create', $id);
+
 		}
 
 		// clear floating thumbnails
