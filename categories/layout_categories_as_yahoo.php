ed593819dc4b301bec6d6a2a5dbe3b17e9265740
Bernard Paques <bernard.paques@gmail.com>
2012-04-12 00:12:31 +0200

    [fix] review signature of get_permalink() member functions across full code

--- categories/layout_categories_as_yahoo.php
+++ categories/layout_categories_as_yahoo.php
@@ -40,7 +40,7 @@ Class Layout_categories_as_yahoo extends Layout_interface {
 		while($item = SQL::fetch($result)) {
 
 			// url to read the full category
-			$url =& Categories::get_permalink($item);
+			$url = Categories::get_permalink($item);
 
 			// initialize variables
 			$prefix = $suffix = $icon = '';
