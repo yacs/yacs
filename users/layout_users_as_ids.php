32d7ffbde47ce54f326c5dd8ea4e8f2d4b5e793c
Bernard Paques <bernard.paques@gmail.com>
2012-08-20 16:46:03 +0200

    the interface shared/anchor.php features a new member function alert_watchers()

---
+++ users/layout_users_as_ids.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * provide only user ids
+ *
+ * @see users/users.php
+ *
+ * @author Bernard Paques
+ * @reference
+ * @license http://www.gnu.org/copyleft/lesser.txt GNU Lesser General Public License
+ */
+Class Layout_users_as_ids extends Layout_interface {
+
+	/**
+	 * list users
+	 *
+	 * @param resource the SQL result
+	 * @return string the rendered text
+	 *
+	 * @see skins/layout.php
+	**/
+	function layout($result) {
+		global $context;
+
+		// we return an array of ids
+		$items = array();
+
+		// empty list
+		if(!SQL::count($result))
+			return $items;
+
+		// process all items in the list
+		while($item = SQL::fetch($result)) {
+
+			// just remember the id
+			$items[] = $item['id'];
+
+		}
+
+		// end of processing
+		SQL::free($result);
+		return $items;
+	}
+
+}
+
+?>
