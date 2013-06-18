fef5e0cd80bbcce7aab35e93832f5d591032c6bf
Alexis Raimbault <rair@cybermedium.info>
2013-06-18 14:25:16 +0200

    [clean] use of XHTML end of self-closing tag cause HTML5 accept it

--- shared/surfer.php
+++ shared/surfer.php
@@ -330,7 +330,7 @@ Class Surfer {
 			$content .= i18n::s('Password').BR.'<input type="password" name="login_password" size="10" maxlength="255" />'.BR;
 
 			// Remember me ?
-			$content .= '<span class="details"><input type="checkbox" name="remember" value="Y" '.EOT.'&nbsp;'.i18n::s('Stay connected').'</span>'.BR;
+			$content .= '<span class="details"><input type="checkbox" name="remember" value="Y" />&nbsp;'.i18n::s('Stay connected').'</span>'.BR;
 
 			// the button
 			$content .= Skin::build_submit_button(i18n::s('Login'));
