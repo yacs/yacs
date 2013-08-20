617687318e8aa9396fb5300ca7eebc9d7e4e8151
Alexis Raimbault <rair@cybermedium.info>
2013-08-20 11:05:54 +0200

    [fix] anchor is null on index

--- categories/categories.php
+++ categories/categories.php
@@ -979,7 +979,7 @@ Class Categories {
 		if($anchor && !is_object($anchor)) {
 		    $anchor = Anchors::get($anchor);
 		}
-		if($anchor->is_assigned())
+		if($anchor && $anchor->is_assigned())
 		    $where .= " OR categories.active='N'";
 					
 
