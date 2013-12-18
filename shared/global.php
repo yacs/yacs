8ffc3b314e11a43f65eb8efe90d224cf0e15d309
Alexis Raimbault <rair@cybermedium.info>
2013-12-18 11:32:23 +0100

    identify text part in modal box

--- shared/global.php
+++ shared/global.php
@@ -1426,8 +1426,8 @@ function render_skin($with_last_modified=TRUE) {
 
 	    if(isset($context['page_header']))
 		echo $context['page_header'];
-	    echo '<h2 class="modal-title">'.$context['page_title'].'</h2>';
-	    echo $context['text'];
+	    echo '<h2 class="modal-title">'.$context['page_title'].'</h2>'."\n";
+	    echo '<div class="modal-text">'.$context['text'].'</div>'."\n";
 	    if(isset($context['javascript']['footer']))
 		echo $context['javascript']['footer'];
 	    return;
