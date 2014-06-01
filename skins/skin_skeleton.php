5fc79c238a86a9f2b0b396367d2831aa0ec8c819
Alexis Raimbault <rair@cybermedium.info>
2014-06-01 18:31:54 +0200

    remove redondant span with button class

--- skins/skin_skeleton.php
+++ skins/skin_skeleton.php
@@ -2949,7 +2949,7 @@ Class Skin_Skeleton {
 			$label = i18n::s('Submit');
 
 		// this is an input button
-		$text = '<span class="button"><button type="submit"';
+		$text = '<button type="submit"';
 
 		// hovering title
 		if($title)
@@ -2971,7 +2971,7 @@ Class Skin_Skeleton {
 		$text .= '>'.$label;
 
 		// end of button
-		$text .= '</button></span>';
+		$text .= '</button>';
 
 		return $text;
 	}
