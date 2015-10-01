0b36882011377b4db673f164d75d077b213ffb2a
Alexis Raimbault <rair@cybermedium.info>
2015-10-01 16:33:56 +0200

    simplify jssor rendering with html slides

--- included/jssor/jssor.php
+++ included/jssor/jssor.php
@@ -75,7 +75,7 @@ Class jssor {
             
             // HTML slide content 
             if(isset($slide['html'])) {
-                $slider .= '<div data-u="slide">'."\n".$slide['html'].'</div>'."\n";
+                $slider .= $slide['html'];
             }
             
             // caption
@@ -97,7 +97,7 @@ Class jssor {
         $slider .= '</div>'."\n";
         
         // javascript initalization
-        $js_options = (isset($options['js']))? $options['js'] : array('$AutoPlay' => 'true');
+        $js_options = (isset($options['js']))? $options['js'] : array('$AutoPlay' => true);
         $rootname   = 'slider'.$slidenum.'_';
         $js_script  = 
               '$(document).ready(function ($) {'."\n"
