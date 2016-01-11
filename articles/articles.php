903848c0fd2257ab7bc72d0e4f4b913ff2aa05f0
Alexis Raimbault <rair@cybermedium.info>
2016-01-11 18:44:21 +0100

    [fix] link to parent section in article notification

--- articles/articles.php
+++ articles/articles.php
@@ -832,7 +832,7 @@ Class Articles {
 			$menu[] = Skin::build_mail_button($link, $label, TRUE);
 
 			// link to the container
-			$link = $context['url_to_home'].$context['url_to_root'].$anchor->get_url();
+			$link = $anchor->get_url();
 			$menu[] = Skin::build_mail_button($link, $anchor->get_title(), FALSE);
 
 		}
