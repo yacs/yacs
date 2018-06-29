f1ffa29aad31f70658d1f0d1ab323edfcbc614c9
Alexis Raimbault <rair@cybermedium.info>
2018-06-29 17:02:00 +0200

    fix columns layout sublists

--- layouts/layout_as_columns/layout_as_columns.php
+++ layouts/layout_as_columns/layout_as_columns.php
@@ -241,7 +241,7 @@ Class Layout_as_columns extends Layout_interface {
                     // layout details
                     if(count($details)) {
                          foreach($details as $line) {
-                             $suffix .= '<div '.tag::_class('details').'>'.YAHOO_ITEM_PREFIX.$line.YAHOO_ITEM_SUFFIX.'</div>';
+                             $suffix .= '<ul '.tag::_class('details').'>'.YAHOO_ITEM_PREFIX.$line.YAHOO_ITEM_SUFFIX.'</ul>';
                          }
                     }
                     
