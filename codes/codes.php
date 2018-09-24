9ab8ba5efb9dfa77e8756a30af62cd12c9dfe2ad
Alexis Raimbault <rair@cybermedium.info>
2018-09-24 10:02:33 +0200

    display youtube and wikipedia with https links

--- codes/codes.php
+++ codes/codes.php
@@ -622,8 +622,8 @@ Class Codes {
                         $patterns_map['/\[(button)=([^\|]+?)\|([^\]]+?)]/is']                       = 'Codes::render_link'      ;  // [button=label|url]
                         $patterns_map['/\[(click)=([^\|]+?)\|([^\]]+?)]/is']                        = 'Codes::render_link'      ;  // [click=label|url]
                         $patterns_map['/(\[)([^ ][^\]\|]+?[^ ])\|([^ ][^\]]+?[^ ])\]/is']           = 'Codes::render_link'      ;  // [label|url]
-                        $patterns_map['/http[s]*:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_\-]+)[a-zA-Z0-9_\-&=]*/i'] = '<iframe class="youtube-player" type="text/html" width="445" height="364" src="http://www.youtube.com/embed/$1" frameborder="0"></iframe>'; // YouTube link
-                        $patterns_map['/http[s]*:\/\/youtu\.be\/([a-zA-Z0-9_\-]+)/i']               = '<iframe class="youtube-player" type="text/html" width="445" height="364" src="http://www.youtube.com/embed/$1" frameborder="0"></iframe>'; // YouTube link too
+                        $patterns_map['/http[s]*:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_\-]+)[a-zA-Z0-9_\-&=]*/i'] = '<iframe class="youtube-player" type="text/html" width="445" height="364" src="https://www.youtube.com/embed/$1" frameborder="0"></iframe>'; // YouTube link
+                        $patterns_map['/http[s]*:\/\/youtu\.be\/([a-zA-Z0-9_\-]+)/i']               = '<iframe class="youtube-player" type="text/html" width="445" height="364" src="https://www.youtube.com/embed/$1" frameborder="0"></iframe>'; // YouTube link too
                         $patterns_map['#(^|\s)([a-z]+?://[a-zA-Z0-9_\-\.\~\/@&;:=%$\?]+)#']                  = 'Codes::render_link'  ;  // make URL clickable
                         $patterns_map['#(^|\s)(www\.[a-z0-9\-]+\.[a-zA-Z0-9_\-\.\~]+(?:/[^,< \r\n\)]*)?)#i'] = 'Codes::render_link'  ;  // web server url                        
                         $patterns_map['/\[clicks=([^\]]+?)]/is']                                    = 'Codes::render_clicks'    ;  // [clicks=url]  // @TODO: put in extension
@@ -2273,7 +2273,7 @@ Class Codes {
 			$language = $context['language'];
 
 		// make a link to the target page
-		$url = 'http://'.$language.'.wikipedia.org/wiki/Special:Search?search='.preg_replace('[\s]', '_', $id);
+		$url = 'https://'.$language.'.wikipedia.org/wiki/Special:Search?search='.preg_replace('[\s]', '_', $id);
 
 		// return a complete anchor
 		$output = Skin::build_link($url, $text, 'wikipedia');
