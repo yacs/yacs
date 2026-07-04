1c4a2b4ffb32cf28132271254d97e056e74b614b
Alexis Raimbault <rair@cybermedium.info>
2026-07-04 16:39:50 +0200

    update scssphp to 1.13

--- included/scss/scss.inc.php
+++ included/scss/scss.inc.php
@@ -1,9 +1,5 @@
 <?php
 
-if (version_compare(PHP_VERSION, '5.6') < 0) {
-    throw new \Exception('scssphp requires PHP 5.6 or above');
-}
-
 if (! class_exists('ScssPhp\ScssPhp\Version')) {
     spl_autoload_register(function ($class) {
         if (0 !== strpos($class, 'ScssPhp\ScssPhp\\')) {
